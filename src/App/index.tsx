a1cbb546e31a23b753c184f6086220e30d9440d2
MadyanArashy <madyan.arashy5@smk.belajar.id>
2025-02-16 20:05:25 +0700

    chore: restructure files

---
+++ src/App/index.tsx
@@ -0,0 +1,16 @@
+import './index.scss'
+
+function App() {
+  return (
+    <>
+    {/* Hero Section */}
+    <div id='hero' className='container'>
+      <p>
+        Welcome! I'm a high schooler with a background in full-stack web development. I have a lot of experience in the basics like HTML, CSS, JavaScript and also PHP, while learning the ins-and-outs of libraries and frameworks such as React and Laravel. I have some experience in building Wordpress sites and mobile applications with react native.
+      </p>
+    </div>
+    </>
+  )
+}
+
+export default App
